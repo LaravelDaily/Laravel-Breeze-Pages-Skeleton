69bc241b47b2451caaf6d2794afeb572f01580f1
David Lun <mysticcode@gmail.com>
2021-11-18 16:52:39 +0200

    Updates template to fix glitches

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,9 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-
-                    <div class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md">
+                <div class="overflow-hidden overflow-x-auto p-6 bg-white border-b border-gray-200">
+                    <div class="min-w-full align-middle">
                         <table class="min-w-full divide-y divide-gray-200 border">
                             <thead>
                             <tr>
@@ -38,7 +37,9 @@
                         </table>
                     </div>
 
-                    {{ $users->links() }}
+                    <div class="mt-2">
+                        {{ $users->links() }}
+                    </div>
 
                 </div>
             </div>
