da243bf51addecc48ec5c1a746156159c34ce4bd
PovilasKorop <povilask007@gmail.com>
2022-01-01 17:00:26 +0200

    Users table heading align left

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,11 +14,11 @@
                         <table class="min-w-full divide-y divide-gray-200 border">
                             <thead>
                             <tr>
-                                <th class="px-6 py-3 bg-gray-50">
-                                    <span class="text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</span>
                                 </th>
-                                <th class="px-6 py-3 bg-gray-50">
-                                    <span class="text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Email</span>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Email</span>
                                 </th>
                             </tr>
                             </thead>
