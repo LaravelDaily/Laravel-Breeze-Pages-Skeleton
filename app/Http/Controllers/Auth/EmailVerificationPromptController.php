897a27434d5b1b8afd2d6e3bd39aca0cd6da135d
krekas <krekas@gmail.com>
2025-04-18 11:16:04 +0300

    Upgrade to Laravel 12

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,7 +15,7 @@ class EmailVerificationPromptController extends Controller
     public function __invoke(Request $request): RedirectResponse|View
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
+                    ? redirect()->intended(route('home', absolute: false))
                     : view('auth.verify-email');
     }
 }
